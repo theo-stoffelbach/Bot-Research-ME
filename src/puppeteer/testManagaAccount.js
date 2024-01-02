1a7e6309be999adbcb03882885e1f9d2ea92f010
Theo Stoffelbach <theo.stoffelbach@ynov.com>
2024-01-02 21:15:04 +0100

    Feat: Success test with Logout

--- src/puppeteer/testManagaAccount.js
+++ src/puppeteer/testManagaAccount.js
@@ -4,18 +4,16 @@ exports.testManageAccount = async (page) => {
     await page.waitForSelector("a.id_button");
     await page.click("a.id_button");
 
+    await page.waitForTimeout(3000);
     const isConnected = await page.$('div.id_signout'); // Test if you're connected
 
-    console.log(isConnected)
-    console.log("-----")
-
+    await console.log(isConnected)
+    await console.log("-----")
 
 
     if (isConnected) {
         console.log("Oui")
         await testLogout(page);
-        //logout
-
     }else {
         console.log("Non")
     }
